d3abfe7ecf4ffa8bf9760209b8899b7e6efeb002
csharp <bieszczyk2@gmail.com>
2023-12-03 15:31:58 +0100

    Added Delete categories tests

---
+++ tests/Modules/FirstModule/ModularMonolith.Modules.FirstModule.BusinessLogic.Tests.Unit/Categories/DeleteCategoryCommandHandlerTests.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using ModularMonolith.Modules.FirstModule.BusinessLogic.Categories.CommandHandlers;
+using ModularMonolith.Modules.FirstModule.Contracts.Categories.Commands;
+using ModularMonolith.Modules.FirstModule.Domain.Entities;
+using ModularMonolith.Modules.FirstModule.Infrastructure.DataAccess.Categories;
+using ModularMonolith.Shared.BusinessLogic.Exceptions;
+using Xunit;
+
+namespace ModularMonolith.Modules.FirstModule.BusinessLogic.Tests.Unit.Categories;
+
+public class DeleteCategoryCommandHandlerTests
+{
+    [Fact]
+    public async Task ShouldDeleteCategory()
+    {
+        // Arrange
+        await using var cnx = CreateDatabase();
+        
+        var existingCategory = new Category { Id = Guid.NewGuid(), Name = "Category 1" };
+
+        cnx.Categories.Add(existingCategory);
+        await cnx.SaveChangesAsync();
+
+        var cmd = new DeleteCategoryCommand(existingCategory.Id);
+        
+        var handler = new DeleteCategoryCommandHandler(cnx);
+
+        // Act
+        await handler.Handle(cmd, default);
+        
+        // Assert
+        var category = await cnx.Categories.FindAsync(existingCategory.Id);
+
+        category.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task ShouldThrowException_WhenCategoryDoesNotExist()
+    {
+        // Arrange
+        await using var cnx = CreateDatabase();
+
+        var cmd = new DeleteCategoryCommand(Guid.NewGuid());
+
+        var handler = new DeleteCategoryCommandHandler(cnx);
+
+        // Act
+        var act = () =>  handler.Handle(cmd, default);
+
+        // Assert
+        await act.Should().ThrowAsync<EntityNotFoundException>();
+    }
+
+    private static CategoryDbContext CreateDatabase()
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<CategoryDbContext>();
+
+        optionsBuilder.UseInMemoryDatabase(Guid.NewGuid().ToString(), opt =>
+        {
+            opt.EnableNullChecks();
+        });
+
+        return new CategoryDbContext(optionsBuilder.Options);
+    }
+}
