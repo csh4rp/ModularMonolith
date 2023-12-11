99db0d15d2ace56daf3f96512d6cdd78d5f34bca
csharp <bieszczyk2@gmail.com>
2023-12-11 22:57:52 +0100

    Event metadata update

--- src/Shared/ModularMonolith.Shared.Infrastructure/Events/BackgroundServices/EventPublisherBackgroundService.cs
+++ src/Shared/ModularMonolith.Shared.Infrastructure/Events/BackgroundServices/EventPublisherBackgroundService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Concurrent;
+using System.Data;
 using System.Diagnostics;
 using MassTransit;
 using MediatR;
@@ -38,9 +39,6 @@ internal sealed class EventPublisherBackgroundService : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        var eventLogMetaData = _eventMetaDataProvider.GetEventLogMetaData();
-        var eventLockMetaData = _eventMetaDataProvider.GetEventLockMetaData();
-        
         for (var i = 0; i < Environment.ProcessorCount; i++)
         {
             _tasks[i] = Task.Factory.StartNew(async () =>
@@ -50,97 +48,43 @@ internal sealed class EventPublisherBackgroundService : BackgroundService
                     await using var connection = _dbConnectionFactory.Create();
                     await connection.OpenAsync(stoppingToken);
 
-                    await using var transaction = await connection.BeginTransactionAsync(stoppingToken);
-
-                    await using var cmd = connection.CreateCommand();
-                    cmd.Transaction = transaction;
-                    cmd.Parameters.AddWithValue("@id", eventInfo.EventLogId);
-                    cmd.CommandText = 
-                        $"""
-                         INSERT INTO {eventLockMetaData.TableName} ({eventLockMetaData.CorrelationIdColumnName}, {eventLockMetaData.AcquiredAtColumnName})
-                         SELECT {eventLogMetaData.IdColumnName}, CURRENT_TIMESTAMP
-                         FROM {eventLogMetaData.TableName}
-                         WHERE {eventLogMetaData.IdColumnName} = @id
-                         AND {eventLogMetaData.CorrelationIdColumnName} IS NOT NULL
-                         FOR UPDATE SKIP LOCKED
-                         ON CONFLICT DO NOTHING
-                         RETURNING 1;
-                         """;
-                    
-                    await using (var reader = await cmd.ExecuteReaderAsync(stoppingToken))
-                    {
-                        if (!reader.HasRows)
-                        {
-                            // Lock was already acquired
-                            continue;
-                        }
-                    }
-                    
-                    cmd.CommandText =
-                        $"""
-                        SELECT *
-                        FROM {eventLogMetaData.TableName}
-                        WHERE {eventLogMetaData.IdColumnName} = @id
-                        FOR UPDATE SKIP LOCKED
-                        """;
-
+                    await using var transaction = await connection.BeginTransactionAsync(IsolationLevel.Serializable, stoppingToken);
 
                     EventLog? eventLog;
                     
-                    await using (var reader = await cmd.ExecuteReaderAsync(stoppingToken))
+                    if (eventInfo.CorrelationId.HasValue)
                     {
-                        if (!reader.HasRows)
-                        {
-                            // Lock was already acquired
-                            continue;
-                        }
-
-                        await reader.ReadAsync(stoppingToken);
-
-                        eventLog= new EventLog
-                        {
-                            Id = default,
-                            Name = default,
-                            Payload = default,
-                            Type = default,
-                            ActivityId = default,
-                            CorrelationId = default,
-                            CreatedAt = default,
-                            OperationName = default,
-                            UserId = default
-                        };
+                        eventLog = await TryAcquireCorrelationLockAsync(transaction, eventInfo, stoppingToken);
                     }
-
+                    else
+                    {
+                        eventLog = await GetValueAsync(transaction, stoppingToken);
+                    }
+                    
+                    if (eventLog is null)
+                    {
+                        continue;
+                    }
+                    
                     try
                     {
-                        await using var scope = _serviceScopeFactory.CreateAsyncScope();
-
-                        var @event = _eventSerializer.Deserialize(eventLog.Type, eventLog.Payload);
-
-                        var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
-
-                        await mediator.Publish(@event, stoppingToken);
+                        await PublishAsync(eventLog, stoppingToken);
+                        
+                        await MarkAsPublishedAsync(transaction, eventInfo, stoppingToken);
                         
                         await transaction.CommitAsync(stoppingToken);
                     }
                     catch (Exception ex)
                     {
-
-                    }
-                    finally
-                    {
                         await transaction.RollbackAsync(stoppingToken);
                     }
                     
-                    
-
                 }
                 
             }, stoppingToken, TaskCreationOptions.LongRunning, TaskScheduler.Default).Unwrap();
         }
 
         await Task.WhenAll(_tasks);
-        
         
         var pipeline = new ResiliencePipelineBuilder()
             .AddRetry(new RetryStrategyOptions
@@ -154,7 +98,138 @@ internal sealed class EventPublisherBackgroundService : BackgroundService
         await pipeline.ExecuteAsync(RunAsync, stoppingToken);
     }
 
-    private async ValueTask RunAsync(CancellationToken cancellationToken)
+    private async Task MarkAsPublishedAsync(NpgsqlTransaction transaction, EventInfo eventInfo, CancellationToken stoppingToken)
+    {
+        var eventLogMetaData = _eventMetaDataProvider.GetEventLogMetaData();
+        var eventLockMetaData = _eventMetaDataProvider.GetEventLogMetaData();
+        
+        await using var batch = transaction.Connection!.CreateBatch();
+
+        if (eventInfo.CorrelationId.HasValue)
+        {
+            var releaseLockCommand = batch.CreateBatchCommand();
+            releaseLockCommand.Parameters.AddWithValue("@correlation_id", eventInfo.CorrelationId.Value);
+            releaseLockCommand.CommandText =
+                $"""
+                DELETE FROM {eventLockMetaData.TableName}
+                WHERE {eventLogMetaData.CorrelationIdColumnName} = @correlation_id;
+                """;
+        }
+        
+        var markAsPublishedCommand = batch.CreateBatchCommand();
+        markAsPublishedCommand.Parameters.AddWithValue("@id", eventInfo.EventLogId);
+        markAsPublishedCommand.CommandText =
+            $"""
+            UPDATE {eventLogMetaData.TableName}
+            SET {eventLogMetaData.PublishedAtColumnName}
+            WHERE {eventLogMetaData.IdColumnName} = @id
+            """;
+
+        _ = await batch.ExecuteNonQueryAsync(stoppingToken);
+    }
+    
+    private async Task<EventLog?> GetValueAsync(NpgsqlTransaction transaction, CancellationToken stoppingToken)
+    {
+        var eventLogMetaData = _eventMetaDataProvider.GetEventLogMetaData();
+        
+        await using var cmd = transaction.Connection!.CreateCommand();
+        cmd.Transaction = transaction;
+                    
+        cmd.CommandText =
+            $"""
+             SELECT *
+             FROM {eventLogMetaData.TableName}
+             WHERE {eventLogMetaData.IdColumnName} = @id
+             FOR UPDATE SKIP LOCKED
+             """;
+        
+        await using var reader = await cmd.ExecuteReaderAsync(stoppingToken);
+        
+        if (!reader.HasRows || !await reader.ReadAsync(stoppingToken))
+        {
+            return null;
+        }
+
+        return await ReadEventLogAsync(reader, eventLogMetaData, stoppingToken);
+    }
+
+    private static async Task<EventLog> ReadEventLogAsync(NpgsqlDataReader reader,
+        EventLogMetaData eventLogMetaData,
+        CancellationToken stoppingToken) =>
+        new()
+        {
+            Id = reader.GetGuid(eventLogMetaData.IdColumnName),
+            Name = reader.GetString(eventLogMetaData.NameColumnName),
+            Payload = reader.GetString(eventLogMetaData.PayloadColumnName),
+            Type = reader.GetString(eventLogMetaData.TypeColumnName),
+            ActivityId = reader.GetString(eventLogMetaData.ActivityIdColumnName),
+            CorrelationId = await reader.IsDBNullAsync(eventLogMetaData.CorrelationIdColumnName, stoppingToken)
+                ? null
+                : reader.GetGuid(eventLogMetaData.CorrelationIdColumnName),
+            CreatedAt = reader.GetDateTime(eventLogMetaData.CreatedAtColumnName),
+            OperationName = reader.GetString(eventLogMetaData.OperationNameColumnName),
+            UserId = await reader.IsDBNullAsync(eventLogMetaData.UserIdColumnName, stoppingToken)
+                ? null
+                : reader.GetGuid(eventLogMetaData.UserIdColumnName)
+        };
+
+    private async Task<EventLog?> TryAcquireCorrelationLockAsync(NpgsqlTransaction transaction, 
+        EventInfo eventInfo,
+        CancellationToken stoppingToken)
+    {
+        var eventLogMetaData = _eventMetaDataProvider.GetEventLogMetaData();
+        var eventLockMetaData = _eventMetaDataProvider.GetEventLockMetaData();
+        
+        await using var batch = transaction.Connection!.CreateBatch();
+        batch.Transaction = transaction;
+        var insertCommand = batch.CreateBatchCommand();
+        insertCommand.Parameters.AddWithValue("@id", eventInfo.EventLogId);
+        insertCommand.Parameters.AddWithValue("@correlation_id", eventInfo.CorrelationId!.Value);
+        
+        insertCommand.CommandText = 
+            $"""
+             INSERT INTO {eventLockMetaData.TableName} ({eventLockMetaData.CorrelationIdColumnName}, {eventLockMetaData.AcquiredAtColumnName})
+             SELECT {eventLogMetaData.IdColumnName}, CURRENT_TIMESTAMP
+             FROM {eventLogMetaData.TableName}
+             WHERE {eventLogMetaData.IdColumnName} = @id
+             AND {eventLogMetaData.CorrelationIdColumnName} NOT IN 
+                 (SELECT {eventLockMetaData.CorrelationIdColumnName}
+                  FROM {eventLockMetaData.TableName}
+                  WHERE {eventLockMetaData.CorrelationIdColumnName} = @correlation_id)
+             FOR UPDATE SKIP LOCKED
+             ON CONFLICT DO NOTHING
+             RETURNING 1;
+             """;
+
+        var readCommand = batch.CreateBatchCommand();
+        readCommand.Parameters.AddWithValue("@id", eventInfo.EventLogId);
+
+        readCommand.CommandText =
+            $"""
+             SELECT *
+             FROM {eventLogMetaData.TableName}
+             WHERE {eventLogMetaData.IdColumnName} = @id
+             FOR UPDATE SKIP LOCKED
+             """;
+
+        await using var reader = await batch.ExecuteReaderAsync(stoppingToken);
+
+        if (!reader.HasRows || !await reader.ReadAsync(stoppingToken))
+        {
+            return null;
+        }
+
+        _ = await reader.NextResultAsync(stoppingToken);
+        
+        if (!reader.HasRows || !await reader.ReadAsync(stoppingToken))
+        {
+            return null;
+        }
+
+        return await ReadEventLogAsync(reader, eventLogMetaData, stoppingToken);
+    }
+
+    private async Task RunAsync(CancellationToken cancellationToken)
     {
         var pipeline = new ResiliencePipelineBuilder()
             .AddRetry(new RetryStrategyOptions
--- src/Shared/ModularMonolith.Shared.Infrastructure/Events/MetaData/EventLogMetaData.cs
+++ src/Shared/ModularMonolith.Shared.Infrastructure/Events/MetaData/EventLogMetaData.cs
@@ -7,4 +7,14 @@ public class EventLogMetaData
     public required string IdColumnName { get; init; }
     
     public required string CorrelationIdColumnName { get; init; }
+    
+    public required string NameColumnName { get; set; }
+    
+    public required string PayloadColumnName { get; set; }
+    public required string TypeColumnName { get; set; }
+    public required string ActivityIdColumnName { get; set; }
+    public required string CreatedAtColumnName { get; set; }
+    public required string OperationNameColumnName { get; set; }
+    public required string UserIdColumnName { get; set; }
+    public required string PublishedAtColumnName { get; set; }
 }
