d6561c1f7f1de9d521c30ca418eb0c8447bcd7ba
csh4rp <bieszczyk2@gmail.com>
2023-12-16 21:03:46 +0100

    Added logging extensions

---
+++ src/Shared/ModularMonolith.Shared.Infrastructure/Logging/ServiceCollectionExensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace ModularMonolith.Shared.Infrastructure.Logging;
+
+public static class ServiceCollectionExensions
+{
+    public static IServiceCollection AddLogging(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddLogging(loggingBuilder =>
+            loggingBuilder.AddSerilog(dispose: true));
+
+        return serviceCollection;
+    }
+}
