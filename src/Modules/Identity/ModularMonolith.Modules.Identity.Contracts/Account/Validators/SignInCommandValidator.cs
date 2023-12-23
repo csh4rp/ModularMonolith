16278563490f631957ff046243af11068bb515ac
csharp <bieszczyk2@gmail.com>
2023-12-23 13:04:06 +0100

    Added signin command validator

--- src/Modules/Identity/ModularMonolith.Modules.Identity.Contracts/Account/Validators/SignInCommandValidator.cs
+++ src/Modules/Identity/ModularMonolith.Modules.Identity.Contracts/Account/Validators/SignInCommandValidator.cs
@@ -1,6 +1,18 @@
-﻿namespace ModularMonolith.Modules.Identity.Contracts.Account.Validators;
+﻿using FluentValidation;
+using ModularMonolith.Modules.Identity.Contracts.Account.Commands;
 
-public class SignInCommandValidator
+namespace ModularMonolith.Modules.Identity.Contracts.Account.Validators;
+
+public class SignInCommandValidator : AbstractValidator<SignInCommand>
 {
-    
+    public SignInCommandValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MaximumLength(128);
+    }
 }
