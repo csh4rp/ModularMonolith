25b58abd15d3dda9c3aa35f126e29692ab8401da
csh4rp <bieszczyk2@gmail.com>
2024-04-19 21:27:10 +0200

    feat: additional files

---
+++ src/Shared/ModularMonolith.Shared.DataAccess.EntityFramework/Transactions/UnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using ModularMonolith.Shared.Application.Abstract;
+
+namespace ModularMonolith.Shared.DataAccess.EntityFramework.Transactions;
+
+public sealed class UnitOfWork : IUnitOfWork
+{
+
+    private readonly IDbContextTransaction _transaction;
+
+    public UnitOfWork(IDbContextTransaction transaction) => _transaction = transaction;
+
+    public Task CompleteAsync(CancellationToken cancellationToken) => _transaction.CommitAsync(cancellationToken);
+
+    public ValueTask DisposeAsync() => _transaction.DisposeAsync();
+}
