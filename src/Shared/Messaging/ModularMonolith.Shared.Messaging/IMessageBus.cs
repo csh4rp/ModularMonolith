c748ec288ccbfeb1443e87468620c3e13adf2fba
csh4rp <bieszczyk2@gmail.com>
2024-04-28 14:22:07 +0200

    feat: directory change

---
+++ src/Shared/Messaging/ModularMonolith.Shared.Messaging/IMessageBus.cs
@@ -0,0 +1,13 @@
+﻿using ModularMonolith.Shared.Contracts;
+using ModularMonolith.Shared.Events;
+
+namespace ModularMonolith.Shared.Messaging;
+
+public interface IMessageBus
+{
+    Task PublishAsync(IEvent @event, CancellationToken cancellationToken);
+
+    Task PublishAsync(IEnumerable<IEvent> events, CancellationToken cancellationToken);
+
+    Task SendAsync(ICommand command, CancellationToken cancellationToken);
+}
